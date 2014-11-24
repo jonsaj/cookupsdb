4bb4890f5850a05801f90289dbb0cb262efd2454
jonsaj <jsaj@umass.edu>
2014-11-24 00:52:56 -0500

    module now does yummly search

---
+++ databaseConsoleInput.js
@@ -0,0 +1,219 @@
+/*
+ * Jon Saj
+ *
+*/
+
+var db = require('./index.js');
+var loginUser = {uid:null};
+//var db = require('./db');
+//process.stdin.setEncoding('utf8');
+
+if (process.argv.length < 3) {
+	console.log("usage: -[flag] params");
+	console.log("  -u    ");
+	console.log("       List All Users"); console.log();
+	console.log("  -ui userID [username] [email] ");
+	console.log("       Get User by Id"); console.log();
+	console.log("  -ue   ");
+	console.log("       Edit User Information");
+	console.log("       ---Note: user's info fields can't contain spaces using this tool---"); console.log();
+	console.log("  -r   ");
+	console.log("       List All Recipes"); console.log();
+	console.log("  -ri recipeID ");
+	console.log("       Get Recipe By ID Number"); console.log();
+	console.log("  -nu [username] [email] [password]  ");
+	console.log("       Add New User"); console.log();
+	console.log("  -nr [userid] [recipename] [instructions] [ingredient] {ingredients}");
+	console.log("       Add new user recipe");
+	console.log("       ---Note: instructions can't contain spaces using this tool---");console.log();
+	console.log("  -i [ingredient] {ingredients}");
+	console.log("       Find By Ingredients"); console.log();
+	console.log("  -l [email] [password]");
+	console.log("       Login User"); console.log();
+	console.log("  -nb [userid] [entrytitle] [text] ");
+	console.log("       Add new blog entry");
+	console.log("       ---Note: text can't contain spaces using this tool---");console.log();
+	console.log("  -bi blogEntryID ");
+	console.log("       Get blog entry By ID Number"); console.log();
+	console.log("  -bui userID ");
+	console.log("       Get blog entry By user ID Number"); console.log();
+
+	console.log("  -bdu userID");
+	console.log("       Delete all blog entries for user id"); console.log();
+	console.log("  -bd [blogid]");
+	console.log("       Delete blog entry by entry id"); console.log();
+	console.log("  -be [blogid] [- | new title] [- | new text]");
+	console.log("       Edit existing blog entry. If no change in title/text, set as -");
+	console.log("       ---Note: text can't contain spaces using this tool---");console.log();
+	console.log("  -re [userid] [recipeid] ");
+	console.log("       Edit recipe id with matching userid");
+	console.log("       ---Note: I'm tired of making a ton of argument inputs. This one replaces a bunch of the recipe with dummy info");
+	console.log("  -s  ");
+	console.log("       fixed search query. I can't spend mroe time making this tool ~robust");
+
+	process.exit(1);
+}
+else if(process.argv[2] === '-s' ){
+	var quer = {};
+	quer.allowedIngredient = ['milk'];
+	//console.log(quer);
+	db.search(quer, function(error, result){
+		if(error) return console.log(error);
+		else console.log(result);
+	});
+}
+else if(process.argv[2] === '-re' && process.argv.length >= 4){
+	var rec = {uid:parseInt(process.argv[3]),
+		 rid:parseInt(process.argv[4]),
+		name:"crummy food",
+		ingredients: ["eggs","milk","butter"],
+		instructions: "make some crummy food, ya baffoon.",
+		servings: 4,
+		meaty: .8,
+		sweet: .1,
+		sour: 1,
+		calories:9992,
+		imagename: "foodimage.png"
+	};
+	db.editRecipe(rec, function(error, result){
+		if(error) return console.log(error);
+		else console.log(result);
+	});
+}
+else if(process.argv[2] === '-ue' && process.argv.length >= 4){
+	var user = {uid:parseInt(process.argv[3])};
+	if(process.argv[4] !== "-" && process.argv[4] != null) user.uname = process.argv[4];
+	if(process.argv[5] !== "-" && process.argv[5] != null) user.email = process.argv[5];
+	if(process.argv[6] !== "-" && process.argv[6] != null) user.password = process.argv[6];
+	if(process.argv[7] !== "-" && process.argv[7] != null) user.bio = process.argv[7];
+	if(process.argv[8] !== "-" && process.argv[8] != null) user.picture = process.argv[8];
+	db.editUser(user, function(error, result){
+		if(error) return console.log(error);
+		else console.log(result);
+	});
+}
+else if(process.argv[2] === '-bdu' && process.argv.length >= 4){
+	var user = {uid:parseInt(process.argv[3])};
+	db.deleteUserBlogById(user.uid, function(error, result){
+		if(error) return console.log(error);
+		else console.log(result);
+	});
+}
+else if(process.argv[2] === '-bui' && process.argv.length >= 4){
+	var user = {uid:parseInt(process.argv[3])};
+	db.getUserBlogById(user.uid, function(error, result){
+		if(error) return console.log(error);
+		else console.log(result);
+	});
+}
+else if(process.argv[2] === '-bi' && process.argv.length >= 4){
+	var blog = {eid:parseInt(process.argv[3])};
+	db.getBlogEntryById(blog.eid, function(error, result){
+		if(error) return console.log(error);
+		else console.log(result);
+	});
+}
+else if(process.argv[2] === '-be' && process.argv.length >= 4){
+	var blog = {eid:parseInt(process.argv[3])};
+	if(process.argv[4] !== "-" && process.argv[4] != null) blog.title = process.argv[4];
+	if(process.argv[5] !== "-" && process.argv[5] != null) blog.extract = process.argv[5];
+	db.editBlogEntry(blog, function(error, result){
+		if(error) return console.log(error);
+		else console.log(result);
+	});
+}
+else if(process.argv[2] === '-bd' && process.argv.length >= 4){
+	var blog = {eid:parseInt(process.argv[3])};
+	db.deleteBlogEntryById(blog.eid, function(error, result){
+		if(error) return console.log(error);
+		else console.log(result);
+	});
+}
+else if(process.argv[2] === '-ri' && process.argv.length >= 4){
+	var rec = {rid:parseInt(process.argv[3])};
+	db.getRecipeById(rec.rid, function(error, result){
+		if(error) return console.log(error);
+		else console.log(result);
+	});
+}
+else if(process.argv[2] === '-ui'){
+	var user = {uid:process.argv[3]};
+	db.getUserById(user.uid, function(error, result){
+		if(error) return console.log(error);
+		else console.log(result);
+	});
+}
+else if(process.argv[2] === '-l'){
+	var user = {email:process.argv[3], password:process.argv[4]};
+	db.login(user, function(error, result){
+		if(error) return console.log(error);
+		else console.log( "User " + result.uid + " logged in. Welcome, " + result.uname );		
+	});
+}
+
+else if(process.argv[2] === '-u'){
+	db.printAllUsers( function(error, result){
+		if(error) return console.log('error', error);
+		console.log(result);
+	});
+}
+
+else if(process.argv[2] === '-r'){
+	db.printAllRecipes( function(error, result){
+		if(error) return console.log('error', error);
+		console.log(result);
+	});
+}
+
+else if(process.argv[2] === '-nu' && process.argv.length >= 6){
+	var newUser = {uname:process.argv[3],email:process.argv[4],password:process.argv[5]};
+	if(process.argv[6]) newUser.bio = process.argv[6];
+	if(process.argv[7]) newUser.picture =  process.argv[7];
+	db.addUser(newUser, function(error, result){
+		if(error) return console.log('error', error);
+		console.log("successfully added " + result.uname + " at Id " + result.uid);
+	});
+
+
+}
+else if(process.argv[2] === '-nr' && process.argv.length >= 6){
+	var recipe = {uid:parseInt(process.argv[3]),name:process.argv[4],instructions:process.argv[5],ingredients:[process.argv[6]]};
+	for(var i = 7; i < process.argv.length; i++){
+		recipe.ingredients[i-6] = process.argv[i];
+	}
+	recipe.bitter = .3;
+	recipe.sweet = .93;
+	recipe.meaty = 1;
+	recipe.salty = .2;
+	recipe.sour = 1;
+	recipe.servings = 100;
+	recipe.totaltime = 12300;
+	recipe.imagename = "recipe.png";
+	db.addRecipe(recipe, function(error, result){
+		if(error) return console.log('error', error);
+		console.log(result);
+	});
+}
+else if(process.argv[2] === '-nb' && process.argv.length >= 4){
+	var entry = {uid:process.argv[3],title:process.argv[4],extract:process.argv[5]};
+	db.addBlogEntry(entry, function(error, result){
+		if(error) return console.log('error', error);
+		console.log(result);
+	});
+}
+else if(process.argv[2] === '-i' && process.argv.length >= 3){
+	var query = {ingredients:[]};
+	for (var i = 3; i < process.argv.length; i++){
+		query.ingredients.push(process.argv[i]);
+	}
+//	console.log(ingreds);
+	db.getByIngredients(query, function(error, result){
+		if(error) return console.log('error', error);
+		
+		console.log(result);
+	});
+}
+else {
+
+console.log("args didn't match");
+}
--- index.js
+++ index.js
@@ -1,442 +1,63 @@
-var pg = require('pg');
-var conString;
-if(process.env.USER === 'cinnamoncrickets'){
-	conString = 'postgres://cinnamoncrickets:cookups.org@localhost/cookupsdb';
-}
-else{
-	conString = 'postgres://'+process.env.USER+':cookups.org@localhost/cookupsdb';
-}
-
-/*
- * args: addUser( newuser, callback(error, result) );
- *
-	Needs: User Exists In System
- *
- */
-
-function login(user, callback){
-
-	var stmtArr = [user.email]; 
-	var stmt = "SELECT uid, email, uname, password, picture FROM users WHERE email = $1";
-	stmt += ";";
-console.log(stmt);
-
-	pg.connect(conString, function(err, client, done){
-		if(err){
-			callback("Internal Database Error");
-			return console.err('err', err);
-		}
-		client.query(stmt, stmtArr, function(err, results){
-			done();
-			if(err){
-				callback("Internal Database Error");
-				return console.err('err', err);
-			}
-			if(! results.rows[0]) {
-				var errmsg = "User not in database";
-				callback(errmsg);
-			}
-			else if(results.rows[0].password === user.password){
-				var success = results.rows[0];
-				delete success.password;
-				callback(err, success );
-			}
-			else{
-				var errmsg = "Incorrect Password";
-				callback(errmsg);
-			}
-			//done();
-		});
-	});
-	pg.end();
-}
-
-function addUser(newuser, callback){
-	var error = null;
-	if( newuser.uname === null || newuser.uname === "")		error = "name can't be empty";
-	else if(newuser.uname.length > 20 ) 	error = "name max size of 20";
-	else if(newuser.password === null || newuser.password === "")	error = "password can't be empty";
-	else if(newuser.password.length > 25)	error = "password max size of 25";
-	else if (newuser.email === null)	error = "email can't be empty";
-	else if (newuser.email.length > 40 ) 	error = "email max size of 40";
-	if (error){
-		callback(error);
-		return;
-	}
-
-	var stmtArr = [newuser.uname, newuser.email, newuser.password];
-
-	var stmt = "INSERT into USERS values(DEFAULT,$1,$2,$3";
-	if(newuser.bio) { stmt += ",$4"; stmtArr.push(newuser.bio); }
- 	if(newuser.picture) { stmt += ",$5"; stmtArr.push(newuser.picture); }
-	stmt += ") RETURNING uid;";
-console.log(stmt);
-		pg.connect(conString,function(err, client, done){
-//		pg.connect(conString, function(err, client, done){
-		if(err){
-			callback(err, error);
-			return console.err('err', err);
-		}	
-		client.query(stmt, stmtArr, function(err, results){
-			done();
-			var returnUser = newuser;
-			delete returnUser.password;
-			returnUser.uid = results.rows[0].uid;
-			callback(err, returnUser);
-		});
-	});
-	pg.end();
-}
-
-function getRecipesByUser(query, callback){
-	
-	var stmt = "SELECT R.name FROM recipes R where ";
-}
-
-function getRecipesByQuery(query, callback){
-	var error = null;
-	var stmt = "SELECT name FROM recipes WHERE ingredients @> '{";
-	for(var i = 0; i < query.ingredients.length; i++){
-		if(i === query.ingredients.length-1) stmt += query.ingredients[i];
-		else stmt += query.ingredients[i] + ",";
-	}
-	stmt += "}'::text[];";
-
-//	console.log("sql: " + stmt);	
-	pg.connect(conString, function(err, client, done){
-		if(err){
-			return console.err('err', err);
-		}
-		client.query(stmt, function(err, results){
-			done();
-			callback(err, results.rows);
-		});
-	});	
-	pg.end();
-}
-
-function getByIngredients(query, callback){
-	var error = null;
-	var stmt = "SELECT name FROM recipes WHERE ingredients @> '{";
-	for(var i = 0; i < query.ingredients.length; i++){
-		if(i === query.ingredients.length-1) stmt += query.ingredients[i];
-		else stmt += query.ingredients[i] + ",";
-	}
-	stmt += "}'::text[];";
-
-//	console.log("sql: " + stmt);	
-	pg.connect(conString, function(err, client, done){
-		if(err){
-			callback("Internal Database Error");
-			return console.err('err', err);
-		}
-		client.query(stmt, function(err, results){
-			done();
-			callback(err, results.rows);
-		});
-	});	
-	pg.end();
-}
-
-function printAllRecipes(callback){
-
-	pg.connect(conString, function(err, client, done){
-		if(err){
-			return console.err('err', err);
-		}
-		client.query('select * from recipes;', function(err, result){
-			done();
-			if(result.rows[0]) callback(err, result.rows);
-			else callback("Recipe Not Found");
-		});
-
-	});	
-	pg.end();
-}
-
-function printAllUsers(callback){
-
-	pg.connect(conString, function(err, client, done){
-		if(err){
-			return console.err('err', err);
-		}
-		client.query('select * from users;', function(err, result){
-			done();
-			callback(err, result.rows);
-		});
-
-	});	
-	pg.end();
-}
-
-function getUserById(userId, callback){
-	var stmtArr = [userId];
-
-	var stmt = "SELECT U.uid, U.uname, U.bio, U.picture FROM users U where U.uid = $1;";
-	
-	pg.connect(conString, function(err, client, done){
-		if(err) return;
-		client.query(stmt, stmtArr, function(err, result){
-			done();
-			callback(err, result.rows);
-		});
-	});
-	pg.end();
-}
-
-/*
-function addUserInfo(userInfo, function(err, client, done){
-	
-	pg.connect(conString, function(err, client, done){
-		
-		var stmt = 	
-	});
-}
-*/
+var db = require('./lib/dbfunctions');
+var ym = require('./lib/yumfunctions');
 
 function getRecipeById(rid, callback){
-
-	var stmtArr = [rid];
-
-	var stmt = "SELECT * FROM recipes WHERE rid = $1;";
-
-	pg.connect(conString, function(err, client, done){
-		if(err){
-			callback("Internal Database Error");
-			return console.err('err',err);
-		}
-		
-		client.query(stmt, stmtArr, function(err, result){
-			done();
-			if(result.rows[0]) callback(err, result.rows[0]);
-			else callback("Recipe Not Found");
-		});
-	});
-	pg.end();
-}
-/*
-	needs imagepath. Default for now. same with flavors.
-*/
-function addRecipe(recipe, callback){
-	var stmtArr = [recipe.name, recipe.ingredients, recipe.instructions];
-	
-	var stmt = "INSERT into RECIPES values(DEFAULT,$1,$2,$3";
-	if(recipe.totalTime) { stmt += ",$4"; stmtArr.push(recipe.totalTime); }
- 	if(recipe.servings) { stmt += ",$5"; stmtArr.push(recipe.servings); }
-	stmt += ") RETURNING rid;";
-
-	pg.connect(conString, function(err, client, done){
-		if(err){
-			callback("Internal Database Error");
-			return console.err('err', err);
-		}
-
-		client.query(stmt, stmtArr, function(err, results){
-			done();
+	if(isNaN(rid) && rid !== undefined){
+		ym.getRecipeById(rid, function(err, result){
 			if(err){
-				callback("Internal Database Error");
-				//done();
-				return console.err('err', err);
+				callback("Problem with Yummly!", result);
+				return console.error("error", err);
 			}
-			//new stmt
-			stmt = "INSERT into USRECIPES values(";
-			stmt += recipe.uid + "," + results.rows[0].rid;
-			stmt += ");";
-//	console.log(stmt);		
-
-			callback(err, results.rows[0]);
-			/*
-				// IMPORTANT: need a way to perform this in one query
-			client.query(stmt, function(err, results_2){
-				done();
-				if(err){
-					callback("Internal Database Error");
-					return console.err('err', err);	
-				}
-				recipe.rid = results.rows[0].rid;
-				callback(err, recipe);
-			});
-			*/
+			callback(err, result);			
 		});
-
-		
-	});
-	pg.end();
-}
-
-function addBlogEntry(entry, callback){
-	var stmtArr = [entry.uid, entry.title, entry.extract];
-
-	var stmt = "INSERT into BLOGS values(DEFAULT,$1,$2,NOW(),$3) RETURNING eid;";
-
-
-	var error = null;
-	if( entry.uid === null ) error = "user must be logged in";
-	else if(entry.title === null || entry.title === "")	error = "blog entry must have a title";
-	else if(entry.extract === null || entry.extract === "")	error = "blog entry must contain some text content";
-	if (error){
-		callback(error);
-		return;
 	}
-/*	var stmt = "INSERT into BLOGS values(DEFAULT,";
-	stmt +=  "'" + entry.uid + "','" + entry.title + "',NOW(),'" + entry.extract + "'";
-	stmt += ") RETURNING eid;";
-*/	
-	pg.connect(conString, function(err, client, done){
-		if(err){
-			callback(err, error);
-			return console.err('err', err);
-		}	
-		client.query(stmt, stmtArr,  function(err, results){
-			done();
-			entry.eid = results.rows[0].eid
-			callback(err, entry);
+	else{
+		db.getRecipeById(rid, function(err, result){
+			if(err){
+				callback(err);
+			}
+			else{
+				callback(err, result);
+			}
 		});
-	});
-	pg.end();
-}
-
-function deleteBlogEntry(eid, callback){
-	var stmtArr = [eid];
-	var stmt = "DELETE FROM blogs WHERE eid = $1;";
-
-	pg.connect(conString, function(err, client, done){
-		if(err){
-			callback("Internal Database Error");
-			return console.err('err',err);
-		}
-		
-/*		var stmt = "DELETE FROM blogs WHERE eid = ";
-		stmt += id + ";"; */
-		client.query(stmt, stmtArr, function(err, result){
-			done();
-			if(err) callback(err);
-			else callback(err, "Deletion successful");
-		});
-	});
-	pg.end();
-}
-
-function editBlogEntry(entry, callback){
-	var stmtArr = [];
-	
-	var stmt = "UPDATE blogs SET ";
-	if(entry.title) { stmtArr.push(entry.title) ; stmt += "title = $" + stmtArr.length ; }
-	if(entry.extract) {
-		if(stmtArr.length > 0) { stmt += "," ; }
-		stmtArr.push(entry.extract); stmt += "extract = $" + stmtArr.length ;
 	}
-	stmtArr.push(entry.eid);
-	stmt += " WHERE eid = $"+ stmtArr.length + ";";
-	var error = null;
-	if(!entry.eid) error = "blog entry must already exist";
-	/*
-	else if(!entry.title || entry.title === "")	error = "blog entry must have a title";
-	else if(!entry.extract || entry.extract === "")	error = "blog entry must contain some text content";
-*/
-	if ( stmtArr.length === 1){
-		callback("No changes made");
-		return;
-	}
-	if (error){
-		callback(error);
-		return;
-	}
-	/*
-	var stmt = "UPDATE blogs SET title='" + entry.title + "',extract='" + entry.extract + "' ";
-	stmt += "WHERE eid=" + entry.eid + ";";
-	*/
-	pg.connect(conString, function(err, client, done){
-		if(err){
-			callback(err, error);
-			return console.err('err', err);
-		}	
-		client.query(stmt, stmtArr, function(err, results){
-			done();
-			callback(err, entry.title);
-		});
-	});
-	pg.end();
 }
 
-function getBlogEntry(eid, callback){
-	var stmtArr = [eid];
-
-	var stmt = "SELECT * FROM blogs WHERE eid = $1;";
-
-
-	pg.connect(conString, function(err, client, done){
-		if(err){
-			callback("Internal Database Error");
-			return console.err('err',err);
-		}
-/*		
-		var stmt = "SELECT * FROM blogs WHERE eid = ";
-		stmt += id + ";";
-*/
-		client.query(stmt, stmtArr, function(err, result){
-			done();
-			if(result.rows[0]) callback(err, result.rows[0]);
-			else callback("Blog entry does not exist");
+function search(query, callback){
+	db.search(query, function(err1, result1){
+		ym.search(query, function(err2, result2){
+			if(err2 && err1) return console.error("yummly error", err2);
+			if(err1){
+				callback(err2, result2);
+				return console.error("database error", err1);
+			}
+			if(err2){
+				callback(err1, result1);
+				return console.error("yummly error", err2);
+			}
+			//no errors. merge results
+			callback(err1, result1.concat( result2 ));
+			//callback( result1 );
 		});
 	});
-	pg.end();
 }
 
-function getUserBlog(uid, callback){
-	var stmtArr = [uid];
-
-	var stmt = "SELECT * FROM blogs WHERE uid = $1;";
-
-	pg.connect(conString, function(err, client, done){
-		if(err){
-			callback("Internal Database Error");
-			return console.err('err',err);
-		}
-
-		client.query(stmt, stmtArr, function(err, result){
-			done();
-			if(result.rows[0]) callback(err, result.rows);
-			else callback("User's blog contains no entries");
-		});
-	});
-	pg.end();
-}
-
-function deleteUserBlog(uid, callback){
-	var stmtArr = [uid];
-
-	var stmt = "DELETE FROM blogs WHERE uid = $1"; 
-
-	pg.connect(conString, function(err, client, done){
-		if(err){
-			callback("Internal Database Error");
-			return console.err('err',err);
-		}
-		
-		client.query(stmt, stmtArr, function(err, result){
-			done();
-			if(err) callback(err);
-			else callback(err, "Deletion successful");
-		});
-	});
-	pg.end();
-}
-
-
-exports.getUserById = getUserById;
-exports.addUser = addUser;
-exports.addRecipe = addRecipe;
-exports.getByIngredients = getByIngredients;
-exports.printAllRecipes = printAllRecipes;
-exports.printAllUsers = printAllUsers;
-exports.getRecipesByQuery = getRecipesByQuery;
-exports.getRecipesByUser = getRecipesByUser;
+exports.getUserById = db.getUserById;
+exports.addUser = db.addUser;
+exports.addRecipe = db.addRecipe;
+exports.getByIngredients = db.getByIngredients;
+exports.printAllRecipes = db.printAllRecipes;
+exports.printAllUsers = db.printAllUsers;
+exports.getRecipesByQuery = db.getRecipesByQuery;
+exports.getRecipesByUser = db.getRecipesByUser;
+exports.login = db.login;
+exports.addBlogEntry = db.addBlogEntry;
+exports.getUserBlogById = db.getUserBlog;
+exports.getBlogEntryById = db.getBlogEntry;
+exports.deleteBlogEntryById = db.deleteBlogEntry;
+exports.deleteUserBlogById = db.deleteUserBlog;
+exports.editBlogEntry = db.editBlogEntry;
+exports.editUser = db.editUser;
+exports.editRecipe = db.editRecipe;
 exports.getRecipeById = getRecipeById;
-exports.login = login;
-exports.addBlogEntry = addBlogEntry;
-exports.getUserBlogById = getUserBlog;
-exports.getBlogEntryById = getBlogEntry;
-exports.deleteBlogEntryById = deleteBlogEntry;
-exports.deleteUserBlogById = deleteUserBlog;
-exports.editBlogEntry = editBlogEntry;
+exports.search = search;
